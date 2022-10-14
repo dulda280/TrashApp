0cf39b7f7f756b56e54daab93e644f7e8962839e
tromborg <64638583+tromborg@users.noreply.github.com>
2022-10-14 12:10:33 +0200

    Some startscreen layout

--- app/src/main/java/dk/seb/trashapp/StartScreen.kt
+++ app/src/main/java/dk/seb/trashapp/StartScreen.kt
@@ -1,6 +1,8 @@
 package dk.seb.trashapp
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -13,7 +15,7 @@ class StartScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start_screen)
-
+        var selectedItem = ""
         val spinner: Spinner = findViewById(R.id.spinner)
         ArrayAdapter.createFromResource(this,
         R.array.kommuner,
@@ -24,7 +26,7 @@ class StartScreen : AppCompatActivity() {
         }
         spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-                val selectedItem = spinner.getItemAtPosition(p2).toString()
+                selectedItem = spinner.getItemAtPosition(p2).toString()
             }
 
             override fun onNothingSelected(p0: AdapterView<*>?) {
@@ -34,6 +36,10 @@ class StartScreen : AppCompatActivity() {
 
         val button: Button = findViewById(R.id.button)
         button.setOnClickListener {
+            val sp = getSharedPreferences("PREFERENCE_NAME", Context.MODE_PRIVATE)
+            var editor = sp.edit()
+            editor.putString("kommune", selectedItem)
+            editor.commit()
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
